e576948b1ca68cb6ce46e5727a8955a3e1bf5f5c
MrLongBeard <umair.hussain96@gmail.com>
2021-02-24 19:38:05 +0500

    [refac] refactor code using prettier

--- specs/pages/KayakPOM.ts
+++ specs/pages/KayakPOM.ts
@@ -14,23 +14,23 @@ class KayakPOM {
   multiCity: any;
   roundTrip: any;
   travelerInfo: any;
-  travelLabel: any
+  travelLabel: any;
   adultCount: any;
   adultCountIncreaseBtn: any;
-  adultCountError: any
-  originCancelbtn: any
-  originAirport:any
-  originOpt:any
-  childCountIncreaseBtn: any
-  searchBtn: any
-  cheapestPrice: any
-  cheapestTime: any
-  bestPrice: any
-  bestTime: any
-  quickestTime: any
-  quickestPrice: any
-  destinationAirport: any
-  destinationOpt:any
+  adultCountError: any;
+  originCancelbtn: any;
+  originAirport: any;
+  originOpt: any;
+  childCountIncreaseBtn: any;
+  searchBtn: any;
+  cheapestPrice: any;
+  cheapestTime: any;
+  bestPrice: any;
+  bestTime: any;
+  quickestTime: any;
+  quickestPrice: any;
+  destinationAirport: any;
+  destinationOpt: any;
   constructor() {}
   setFlightTab() {
     this.flightTab = element(
@@ -44,19 +44,27 @@ class KayakPOM {
     this.flightTab.click();
   }
   setOrigin() {
-    return this.origin = element(by.xpath("//div[contains(@id,'origin-airport-display')]"));
+    return (this.origin = element(
+      by.xpath("//div[contains(@id,'origin-airport-display')]")
+    ));
   }
   async isOriginDisplayed() {
     return await this.origin.isDisplayed();
   }
-  async getOriginValue(){
-      return await element(by.xpath("//div[contains(@id,'origin-airport-display-multi-container')]/div")).getAttribute('data-value') //this.origin.getAttribute('value')
+  async getOriginValue() {
+    return await element(
+      by.xpath(
+        "//div[contains(@id,'origin-airport-display-multi-container')]/div"
+      )
+    ).getAttribute("data-value"); //this.origin.getAttribute('value')
   }
   setDestination() {
-    this.destination = element(by.xpath("//div[contains(@id,'destination-airport-display')]"));
+    this.destination = element(
+      by.xpath("//div[contains(@id,'destination-airport-display')]")
+    );
   }
-  clickDestination(){
-      this.destination.click()
+  clickDestination() {
+    this.destination.click();
   }
   async isDestinationDisplayed() {
     return await this.destination.isDisplayed();
@@ -83,9 +91,9 @@ class KayakPOM {
   }
   setTripType() {
     this.tripType = element(
-        by.xpath("//*[contains(@id,'switch-display-status')]")
-      )
-    return this.tripType
+      by.xpath("//*[contains(@id,'switch-display-status')]")
+    );
+    return this.tripType;
   }
   async getTripTypeDataAttribute() {
     return await this.tripType.getAttribute("data-value");
@@ -122,11 +130,15 @@ class KayakPOM {
       by.xpath("//button[contains(@id,'travelersAboveForm-dialog-trigger')]")
     );
   }
-  setTravelLabel(){
-      this.travelLabel = element(by.xpath("//button[contains(@id,'travelersAboveForm-dialog-trigger')]/span[2]"))
+  setTravelLabel() {
+    this.travelLabel = element(
+      by.xpath(
+        "//button[contains(@id,'travelersAboveForm-dialog-trigger')]/span[2]"
+      )
+    );
   }
-  async getTravelLabelValue(){
-      return await this.travelLabel.getAttribute('aria-label')
+  async getTravelLabelValue() {
+    return await this.travelLabel.getAttribute("aria-label");
   }
   clickTravelerInfo() {
     this.travelerInfo.click();
@@ -136,101 +148,141 @@ class KayakPOM {
       by.xpath("//input[contains(@id,'travelersAboveForm-adults-input')]")
     );
   }
-  setAdultCountIncreaseBtn(){
-      this.adultCountIncreaseBtn = element(by.xpath("//div[contains(@id,'travelersAboveForm-adults')]/div/div[3]/button"))
+  setAdultCountIncreaseBtn() {
+    this.adultCountIncreaseBtn = element(
+      by.xpath(
+        "//div[contains(@id,'travelersAboveForm-adults')]/div/div[3]/button"
+      )
+    );
   }
-  clickAdultCountIncreaseBtn(){
-      this.adultCountIncreaseBtn.click()
+  clickAdultCountIncreaseBtn() {
+    this.adultCountIncreaseBtn.click();
   }
   increaseAdultCount() {
     this.adultCount.clear().sendKeys("9");
   }
-  setAdultCountError(){
-      this.adultCountError = element(by.xpath("//strong[contains(@id,'travelersAboveForm-errorMessageText')]"))
+  setAdultCountError() {
+    this.adultCountError = element(
+      by.xpath("//strong[contains(@id,'travelersAboveForm-errorMessageText')]")
+    );
   }
-  async getAdultCountError(){
-      return await this.adultCountError.getText()
+  async getAdultCountError() {
+    return await this.adultCountError.getText();
   }
-  setOriginCancelBtn(){
-      this.originCancelbtn = element(by.xpath("//*[contains(@id,'origin-airport-display-multi-container')]/div/div[2]/button"))
+  setOriginCancelBtn() {
+    this.originCancelbtn = element(
+      by.xpath(
+        "//*[contains(@id,'origin-airport-display-multi-container')]/div/div[2]/button"
+      )
+    );
   }
-  clickOriginCancelBtn(){
-      this.originCancelbtn.click()
+  clickOriginCancelBtn() {
+    this.originCancelbtn.click();
   }
-  setOriginAirport(){
-      this.originAirport = element(by.xpath("//input[contains(@id,'origin-airport')]"))
+  setOriginAirport() {
+    this.originAirport = element(
+      by.xpath("//input[contains(@id,'origin-airport')]")
+    );
   }
-  sendOriginText(value:string){
-      this.originAirport.sendKeys(value)
+  sendOriginText(value: string) {
+    this.originAirport.sendKeys(value);
   }
-  setOriginOpt(){
-      this.originOpt = element(by.xpath("//div[contains(@id,'origin-airport-smartbox-dropdown')]/ul/li[1]"))
+  setOriginOpt() {
+    this.originOpt = element(
+      by.xpath(
+        "//div[contains(@id,'origin-airport-smartbox-dropdown')]/ul/li[1]"
+      )
+    );
   }
-  clickOriginOpt(){
-      this.originOpt.click()
+  clickOriginOpt() {
+    this.originOpt.click();
   }
-  setChildIncreaseBtn(){
-      this.childCountIncreaseBtn = element(by.xpath("//div[contains(@id,'travelersAboveForm-child')]/div/div[3]/button"))
+  setChildIncreaseBtn() {
+    this.childCountIncreaseBtn = element(
+      by.xpath(
+        "//div[contains(@id,'travelersAboveForm-child')]/div/div[3]/button"
+      )
+    );
   }
-  clickChildIcreaseBtn(){
-      this.childCountIncreaseBtn.click()
+  clickChildIcreaseBtn() {
+    this.childCountIncreaseBtn.click();
   }
-  setSearchBtn(){
-      this.searchBtn = element(by.xpath("//form[contains(@id,'RTOWsearchform')]/div/div/div[2]/button"))
+  setSearchBtn() {
+    this.searchBtn = element(
+      by.xpath("//form[contains(@id,'RTOWsearchform')]/div/div/div[2]/button")
+    );
   }
-  clickSearchBtn(){
-      this.searchBtn.click()
+  clickSearchBtn() {
+    this.searchBtn.click();
   }
-  setCheapestPrice(){
-     return this.cheapestPrice = element(by.xpath("//*[contains(@id,'tabs')]/a[1]/div/div/div/div[2]/span[1]"))
+  setCheapestPrice() {
+    return (this.cheapestPrice = element(
+      by.xpath("//*[contains(@id,'tabs')]/a[1]/div/div/div/div[2]/span[1]")
+    ));
   }
-  async getCheapestPrice(){
-      return await this.cheapestPrice.getText()
+  async getCheapestPrice() {
+    return await this.cheapestPrice.getText();
   }
-  setCheapestTime(){
-      this.cheapestTime = element(by.xpath("//*[contains(@id,'tabs')]/a[1]/div/div/div/div[2]/span[2]"))
+  setCheapestTime() {
+    this.cheapestTime = element(
+      by.xpath("//*[contains(@id,'tabs')]/a[1]/div/div/div/div[2]/span[2]")
+    );
   }
-  async getCheapestTime(){
-    return await this.cheapestTime.getText()
+  async getCheapestTime() {
+    return await this.cheapestTime.getText();
   }
-  setBestPrice(){
-      this.bestPrice = element(by.xpath("//*[contains(@id,'tabs')]/a[2]/div/div/div/div[2]/span[1]"))
+  setBestPrice() {
+    this.bestPrice = element(
+      by.xpath("//*[contains(@id,'tabs')]/a[2]/div/div/div/div[2]/span[1]")
+    );
   }
-  async getBestPrice(){
-      return await this.bestPrice.getText() 
+  async getBestPrice() {
+    return await this.bestPrice.getText();
   }
-  setBestTime(){
-      this.bestTime = element(by.xpath("//*[contains(@id,'tabs')]/a[2]/div/div/div/div[2]/span[2]"))
+  setBestTime() {
+    this.bestTime = element(
+      by.xpath("//*[contains(@id,'tabs')]/a[2]/div/div/div/div[2]/span[2]")
+    );
   }
-  async getBestTime(){
-      return await this.bestTime.getText()
+  async getBestTime() {
+    return await this.bestTime.getText();
   }
-  setQuickestTime(){
-      this.quickestTime = element(by.xpath("//*[contains(@id,'tabs')]/a[3]/div/div/div/div[2]/span[2]"))
+  setQuickestTime() {
+    this.quickestTime = element(
+      by.xpath("//*[contains(@id,'tabs')]/a[3]/div/div/div/div[2]/span[2]")
+    );
   }
-  async getQuickestTime(){
-      return await this.quickestTime.getText()
+  async getQuickestTime() {
+    return await this.quickestTime.getText();
   }
-  setQuickestPrice(){
-      this.quickestPrice = element(by.xpath("//*[contains(@id,'tabs')]/a[3]/div/div/div/div[2]/span[1]"))
+  setQuickestPrice() {
+    this.quickestPrice = element(
+      by.xpath("//*[contains(@id,'tabs')]/a[3]/div/div/div/div[2]/span[1]")
+    );
   }
-  async getQuickestPrice(){
-      return await this.quickestPrice.getText()
+  async getQuickestPrice() {
+    return await this.quickestPrice.getText();
   }
-  setDestinationAirport(){
-      this.destinationAirport = element(by.xpath("//input[contains(@id,'destination-airport')]"))
+  setDestinationAirport() {
+    this.destinationAirport = element(
+      by.xpath("//input[contains(@id,'destination-airport')]")
+    );
   }
-  async getDestinationAirportValue(){
-      return await this.destinationAirport.getAttribute('value')
+  async getDestinationAirportValue() {
+    return await this.destinationAirport.getAttribute("value");
   }
-  sendDestinationAirportValue(value:string){
-    this.destinationAirport.sendKeys(value)
+  sendDestinationAirportValue(value: string) {
+    this.destinationAirport.sendKeys(value);
   }
-  setDestinationOpt(){
-      this.destinationOpt = element(by.xpath("//div[contains(@id,'destination-airport-smartbox-dropdown')]/ul/li[1]"))
+  setDestinationOpt() {
+    this.destinationOpt = element(
+      by.xpath(
+        "//div[contains(@id,'destination-airport-smartbox-dropdown')]/ul/li[1]"
+      )
+    );
   }
-  clickDestinationOpt(){
-      this.destinationOpt.click()
+  clickDestinationOpt() {
+    this.destinationOpt.click();
   }
 }
 // export default new KayakPOM()
--- specs/tests/KayakTest.ts
+++ specs/tests/KayakTest.ts
@@ -1,200 +1,210 @@
-import { browser, element, by, ExpectedConditions } from "protractor"
+import { browser, element, by, ExpectedConditions } from "protractor";
 
-const KayakPOM = require('../pages/KayakPOM')
-describe('Kayak Flight Test', function(){
-    beforeEach(function(){
-        browser.get('https://www.kayak.com')
-        browser.driver.manage().window().maximize();
-        KayakPOM.setFlightTab()
-        KayakPOM.clickFlightTab()
-    })
-    // //step 1
-    it('Test Fields Exist', async function(){
-        KayakPOM.setOrigin()
-        const originResult = await KayakPOM.isOriginDisplayed()
-        KayakPOM.setDestination()
-        const destinationResult = await KayakPOM.isDestinationDisplayed()
-        KayakPOM.setDepartureDate()
-        const departureDateResult = await KayakPOM.isDepartureDateDisplayed()
-        KayakPOM.setReturnDate()
-        const returnDateResult = await KayakPOM.isReturnDateDisplayed()
-        expect(originResult && destinationResult && departureDateResult && returnDateResult).toBeTruthy()
-    })
-    it('Trip Type Test', async function(){
-        KayakPOM.setTripType()
-        let result = await KayakPOM.getTripTypeDataAttribute()
-        expect(result).toBe('roundtrip')    
-    })
-    //step 2
-    it('Set Trip Type Oneway Test',async function(){
-        KayakPOM.setTripType()
-        KayakPOM.clickTripType()
-        KayakPOM.setOneway()
-        KayakPOM.clickOneway()
-        let result = await KayakPOM.getTripTypeDataAttribute()
-        expect(result).toBe('oneway')
-    })
-    //step 3
-    it('Set Trip Type Multi-City Test', async function(){
-        KayakPOM.setTripType()
-        KayakPOM.clickTripType()
-        KayakPOM.setMultiCity()
-        KayakPOM.clickMultiCity()
-        let result = await KayakPOM.getTripTypeDataAttribute()
-        expect(result).toBe('multicity')
-    })
-    //step 4
-    it('Set Trip Type Round Trip Test', async function(){
-        KayakPOM.setTripType()
-        KayakPOM.clickTripType() 
-        KayakPOM.setRoundTrip()
-        KayakPOM.clickRoundTrip()   
-        let result = await KayakPOM.getTripTypeDataAttribute()
-        expect(result).toBe('roundtrip')
-    })
-    // step 5
-    it("Adult Count Test", async function(){
-        KayakPOM.setTravelerInfo()
-        KayakPOM.clickTravelerInfo()
-        KayakPOM.setAdultCountIncreaseBtn()
-        Array.from(new Array(9),()=>{
-            KayakPOM.clickAdultCountIncreaseBtn()
-        })
-        KayakPOM.setAdultCountError()
-        let result = await KayakPOM.getAdultCountError()
-        expect(result).toBe('Searches cannot have more than 9 adults')
-    })
-    //Search Flow
-    //Step 6
-    it('Origin Test PAR', async function(){
-        browser.sleep(1000)
-        KayakPOM.setOriginCancelBtn()
-        KayakPOM.clickOriginCancelBtn()
-        browser.sleep(1000)
-        KayakPOM.setOriginAirport()
-        KayakPOM.sendOriginText('PAR')
-        browser.sleep(1000)
-        KayakPOM.setOriginOpt()
-        KayakPOM.clickOriginOpt()
-        browser.sleep(1000)
-        KayakPOM.setOrigin()
-        let result = await KayakPOM.getOriginValue()
-        console.log('result=> ',result)
-        expect(result).toBe('PAR')
-    })
+const KayakPOM = require("../pages/KayakPOM");
+describe("Kayak Flight Test", function () {
+  beforeEach(function () {
+    browser.get("https://www.kayak.com");
+    browser.driver.manage().window().maximize();
+    KayakPOM.setFlightTab();
+    KayakPOM.clickFlightTab();
+  });
+  // //step 1
+  it("Test Fields Exist", async function () {
+    KayakPOM.setOrigin();
+    const originResult = await KayakPOM.isOriginDisplayed();
+    KayakPOM.setDestination();
+    const destinationResult = await KayakPOM.isDestinationDisplayed();
+    KayakPOM.setDepartureDate();
+    const departureDateResult = await KayakPOM.isDepartureDateDisplayed();
+    KayakPOM.setReturnDate();
+    const returnDateResult = await KayakPOM.isReturnDateDisplayed();
+    expect(
+      originResult &&
+        destinationResult &&
+        departureDateResult &&
+        returnDateResult
+    ).toBeTruthy();
+  });
+  it("Trip Type Test", async function () {
+    KayakPOM.setTripType();
+    let result = await KayakPOM.getTripTypeDataAttribute();
+    expect(result).toBe("roundtrip");
+  });
+  //step 2
+  it("Set Trip Type Oneway Test", async function () {
+    KayakPOM.setTripType();
+    KayakPOM.clickTripType();
+    KayakPOM.setOneway();
+    KayakPOM.clickOneway();
+    let result = await KayakPOM.getTripTypeDataAttribute();
+    expect(result).toBe("oneway");
+  });
+  //step 3
+  it("Set Trip Type Multi-City Test", async function () {
+    KayakPOM.setTripType();
+    KayakPOM.clickTripType();
+    KayakPOM.setMultiCity();
+    KayakPOM.clickMultiCity();
+    let result = await KayakPOM.getTripTypeDataAttribute();
+    expect(result).toBe("multicity");
+  });
+  //step 4
+  it("Set Trip Type Round Trip Test", async function () {
+    KayakPOM.setTripType();
+    KayakPOM.clickTripType();
+    KayakPOM.setRoundTrip();
+    KayakPOM.clickRoundTrip();
+    let result = await KayakPOM.getTripTypeDataAttribute();
+    expect(result).toBe("roundtrip");
+  });
+  // step 5
+  it("Adult Count Test", async function () {
+    KayakPOM.setTravelerInfo();
+    KayakPOM.clickTravelerInfo();
+    KayakPOM.setAdultCountIncreaseBtn();
+    Array.from(new Array(9), () => {
+      KayakPOM.clickAdultCountIncreaseBtn();
+    });
+    KayakPOM.setAdultCountError();
+    let result = await KayakPOM.getAdultCountError();
+    expect(result).toBe("Searches cannot have more than 9 adults");
+  });
+  //Search Flow
+  //Step 6
+  it("Origin Test PAR", async function () {
+    browser.sleep(1000);
+    KayakPOM.setOriginCancelBtn();
+    KayakPOM.clickOriginCancelBtn();
+    browser.sleep(1000);
+    KayakPOM.setOriginAirport();
+    KayakPOM.sendOriginText("PAR");
+    browser.sleep(1000);
+    KayakPOM.setOriginOpt();
+    KayakPOM.clickOriginOpt();
+    browser.sleep(1000);
+    KayakPOM.setOrigin();
+    let result = await KayakPOM.getOriginValue();
+    console.log("result=> ", result);
+    expect(result).toBe("PAR");
+  });
+  //step 7
+  it("Origin Test NYC", async function () {
+    KayakPOM.setDestination();
+    KayakPOM.clickDestination();
+    browser.sleep(1000);
+    KayakPOM.setDestinationAirport();
+    KayakPOM.sendDestinationAirportValue("Paris (PAR)");
+    browser.sleep(2000);
+    KayakPOM.setDestinationOpt();
+    KayakPOM.clickDestinationOpt();
+    const destinationAirport = await KayakPOM.getDestinationAirportValue();
+    expect(destinationAirport).toBe("Paris (PAR)");
+  });
+  ////step 8
+  it("Travel Label Test", async function () {
+    KayakPOM.setTravelerInfo();
+    KayakPOM.clickTravelerInfo();
+    KayakPOM.setAdultCountIncreaseBtn();
+    KayakPOM.clickAdultCountIncreaseBtn();
+    KayakPOM.clickAdultCountIncreaseBtn();
+    KayakPOM.clickAdultCountIncreaseBtn();
+    browser.sleep(1000);
+    KayakPOM.setTravelLabel();
+    let result = await KayakPOM.getTravelLabelValue();
+    expect(result).toBe("4 Travelers");
+  });
+  // //Step 9
+  it("Travel Label Test", async function () {
+    KayakPOM.setTravelerInfo();
+    KayakPOM.clickTravelerInfo();
+    KayakPOM.setAdultCountIncreaseBtn();
+    KayakPOM.clickAdultCountIncreaseBtn();
+    KayakPOM.clickAdultCountIncreaseBtn();
+    KayakPOM.clickAdultCountIncreaseBtn();
+    KayakPOM.setChildIncreaseBtn();
+    KayakPOM.clickChildIcreaseBtn();
+    KayakPOM.clickChildIcreaseBtn();
+    browser.sleep(1000);
+    KayakPOM.setTravelLabel();
+    let result = await KayakPOM.getTravelLabelValue();
+    expect(result).toBe("6 Travelers");
+  });
+  //step 13
+  it("Complete Search Flow", async function () {
+    //step 6
+    browser.sleep(1000);
+    KayakPOM.setOriginCancelBtn();
+    KayakPOM.clickOriginCancelBtn();
+    browser.sleep(1000);
+    KayakPOM.setOriginAirport();
+    KayakPOM.sendOriginText("PAR");
+    browser.sleep(1000);
+    KayakPOM.setOriginOpt();
+    KayakPOM.clickOriginOpt();
     //step 7
-    it('Origin Test NYC', async function(){
-        KayakPOM.setDestination()
-        KayakPOM.clickDestination()
-        browser.sleep(1000)
-        KayakPOM.setDestinationAirport()
-        KayakPOM.sendDestinationAirportValue('Paris (PAR)')
-        browser.sleep(2000)
-        KayakPOM.setDestinationOpt()
-        KayakPOM.clickDestinationOpt()
-        const destinationAirport = await KayakPOM.getDestinationAirportValue()
-        expect(destinationAirport).toBe('Paris (PAR)')
-    })
-    ////step 8
-    it('Travel Label Test', async function(){
-        KayakPOM.setTravelerInfo()
-        KayakPOM.clickTravelerInfo()
-        KayakPOM.setAdultCountIncreaseBtn()
-        KayakPOM.clickAdultCountIncreaseBtn()
-        KayakPOM.clickAdultCountIncreaseBtn()
-        KayakPOM.clickAdultCountIncreaseBtn()
-        browser.sleep(1000)
-        KayakPOM.setTravelLabel()
-        let result = await KayakPOM.getTravelLabelValue()
-        expect(result).toBe('4 Travelers')
-    })
-    // //Step 9
-    it('Travel Label Test', async function(){
-        KayakPOM.setTravelerInfo()
-        KayakPOM.clickTravelerInfo()
-        KayakPOM.setAdultCountIncreaseBtn()
-        KayakPOM.clickAdultCountIncreaseBtn()
-        KayakPOM.clickAdultCountIncreaseBtn()
-        KayakPOM.clickAdultCountIncreaseBtn()
-        KayakPOM.setChildIncreaseBtn()
-        KayakPOM.clickChildIcreaseBtn()
-        KayakPOM.clickChildIcreaseBtn()
-        browser.sleep(1000)
-        KayakPOM.setTravelLabel()
-        let result = await KayakPOM.getTravelLabelValue()
-        expect(result).toBe('6 Travelers')
-    })
+    KayakPOM.setDestination();
+    KayakPOM.clickDestination();
+    browser.sleep(1000);
+    KayakPOM.setDestinationAirport();
+    KayakPOM.sendDestinationAirportValue("NYC");
+    browser.sleep(2000);
+    KayakPOM.setDestinationOpt();
+    KayakPOM.clickDestinationOpt();
+    browser.sleep(1000);
+    //step 8
+    KayakPOM.setTravelerInfo();
+    KayakPOM.clickTravelerInfo();
+    KayakPOM.setAdultCountIncreaseBtn();
+    KayakPOM.clickAdultCountIncreaseBtn();
+    KayakPOM.clickAdultCountIncreaseBtn();
+    KayakPOM.clickAdultCountIncreaseBtn();
+    browser.sleep(1000);
+    //step 9
+    KayakPOM.setChildIncreaseBtn();
+    KayakPOM.clickChildIcreaseBtn();
+    KayakPOM.clickChildIcreaseBtn();
+
     //step 13
-    it('Complete Search Flow', async function(){
-        //step 6
-        browser.sleep(1000)
-        KayakPOM.setOriginCancelBtn()
-        KayakPOM.clickOriginCancelBtn()
-        browser.sleep(1000)
-        KayakPOM.setOriginAirport()
-        KayakPOM.sendOriginText('PAR')
-        browser.sleep(1000)
-        KayakPOM.setOriginOpt()
-        KayakPOM.clickOriginOpt()
-        //step 7
-        KayakPOM.setDestination()
-        KayakPOM.clickDestination()
-        browser.sleep(1000)
-        KayakPOM.setDestinationAirport()
-        KayakPOM.sendDestinationAirportValue('NYC')
-        browser.sleep(2000)
-        KayakPOM.setDestinationOpt()
-        KayakPOM.clickDestinationOpt()
-        browser.sleep(1000)
-        //step 8
-        KayakPOM.setTravelerInfo()
-        KayakPOM.clickTravelerInfo()
-        KayakPOM.setAdultCountIncreaseBtn()
-        KayakPOM.clickAdultCountIncreaseBtn()
-        KayakPOM.clickAdultCountIncreaseBtn()
-        KayakPOM.clickAdultCountIncreaseBtn()
-        browser.sleep(1000)
-        //step 9
-        KayakPOM.setChildIncreaseBtn()
-        KayakPOM.clickChildIcreaseBtn()
-        KayakPOM.clickChildIcreaseBtn()
-        
-        //step 13
-        KayakPOM.setSearchBtn()
-        KayakPOM.clickSearchBtn()
-        let e = KayakPOM.setCheapestPrice()
-        browser.wait(ExpectedConditions.presenceOf(e),6000).then(async ()=>{
-            browser.sleep(10000)
-            let cheapestPrice :string = await KayakPOM.getCheapestPrice()
-            const cheapestPriceResult:Number = parseInt(cheapestPrice.slice(1,))
-            console.log('cheapestPriceResult=> ',cheapestPriceResult)
-            KayakPOM.setBestPrice()
-            const bestPrice:string = await KayakPOM.getBestPrice()
-            const bestPriceResult:Number = parseInt(bestPrice.slice(1,))
-            console.log('bestPriceResult=> ',bestPriceResult)
-            KayakPOM.setQuickestPrice()
-            const quickestPrice:string = await KayakPOM.getQuickestPrice()
-            const quickestPriceResult = parseInt(quickestPrice.slice(1,))
-            console.log('quickestPriceResult=> ',quickestPriceResult)
-            KayakPOM.setCheapestTime()
-            let cheapestTime: string = await KayakPOM.getCheapestTime()
-            const cheapestTimeSeconds = durationToSeconds(cheapestTime)
-            console.log('cheapestTimeSeconds=> ',cheapestTimeSeconds)
-            KayakPOM.setBestTime()
-            const bestTime:string = await KayakPOM.getBestTime()
-            const bestTimeSeconds = durationToSeconds(bestTime)
-            console.log('bestTimeSeconds=> ',bestTimeSeconds)
-            KayakPOM.setQuickestTime()
-            const quickestTime:string = await KayakPOM.getQuickestTime()
-            const quickestTimeSeconds = durationToSeconds(quickestTime)
-            console.log('quickestTimeSeconds=> ',quickestTimeSeconds)
-            browser.sleep(5000)
-            expect(cheapestPriceResult <= bestPriceResult && cheapestPriceResult < quickestPriceResult && quickestTimeSeconds <= bestTimeSeconds && quickestTimeSeconds < cheapestTimeSeconds).toBeTruthy()
-        })
-    })
-})
-function durationToSeconds(time:string){
-    let hrs = parseInt(time.split(' ')[0].slice(0,2))
-    let mins = parseInt(time.split(' ')[1].slice(0,2))
-    return (hrs*60*60+mins*60)
+    KayakPOM.setSearchBtn();
+    KayakPOM.clickSearchBtn();
+    let e = KayakPOM.setCheapestPrice();
+    browser.wait(ExpectedConditions.presenceOf(e), 6000).then(async () => {
+      browser.sleep(10000);
+      let cheapestPrice: string = await KayakPOM.getCheapestPrice();
+      const cheapestPriceResult: Number = parseInt(cheapestPrice.slice(1));
+      console.log("cheapestPriceResult=> ", cheapestPriceResult);
+      KayakPOM.setBestPrice();
+      const bestPrice: string = await KayakPOM.getBestPrice();
+      const bestPriceResult: Number = parseInt(bestPrice.slice(1));
+      console.log("bestPriceResult=> ", bestPriceResult);
+      KayakPOM.setQuickestPrice();
+      const quickestPrice: string = await KayakPOM.getQuickestPrice();
+      const quickestPriceResult = parseInt(quickestPrice.slice(1));
+      console.log("quickestPriceResult=> ", quickestPriceResult);
+      KayakPOM.setCheapestTime();
+      let cheapestTime: string = await KayakPOM.getCheapestTime();
+      const cheapestTimeSeconds = durationToSeconds(cheapestTime);
+      console.log("cheapestTimeSeconds=> ", cheapestTimeSeconds);
+      KayakPOM.setBestTime();
+      const bestTime: string = await KayakPOM.getBestTime();
+      const bestTimeSeconds = durationToSeconds(bestTime);
+      console.log("bestTimeSeconds=> ", bestTimeSeconds);
+      KayakPOM.setQuickestTime();
+      const quickestTime: string = await KayakPOM.getQuickestTime();
+      const quickestTimeSeconds = durationToSeconds(quickestTime);
+      console.log("quickestTimeSeconds=> ", quickestTimeSeconds);
+      browser.sleep(5000);
+      expect(
+        cheapestPriceResult <= bestPriceResult &&
+          cheapestPriceResult < quickestPriceResult &&
+          quickestTimeSeconds <= bestTimeSeconds &&
+          quickestTimeSeconds < cheapestTimeSeconds
+      ).toBeTruthy();
+    });
+  });
+});
+function durationToSeconds(time: string) {
+  let hrs = parseInt(time.split(" ")[0].slice(0, 2));
+  let mins = parseInt(time.split(" ")[1].slice(0, 2));
+  return hrs * 60 * 60 + mins * 60;
 }
